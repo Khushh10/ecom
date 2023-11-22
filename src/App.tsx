f8574fc7f4d42c1df375b5223001a07d8f456a58
Khush <tatwavedikhush@gmail.com>
2023-11-22 17:29:07 +0530

    new

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function MyButton() {
+  return (
+    <button>Sample Button</button>
+  );
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Welcome To React</h1>
+          <p>~Khush</p>
+          <p><MyButton /></p>
+        </div>
       </header>
     </div>
   );
